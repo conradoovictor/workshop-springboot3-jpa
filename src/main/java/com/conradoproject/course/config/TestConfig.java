c38e53dd9446687902d66a8842ee91e5256e74b4
conradoovictor <conrado.victor01@gmail.com>
2025-10-27 11:26:31 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/conradoproject/course/config/TestConfig.java
+++ src/main/java/com/conradoproject/course/config/TestConfig.java
@@ -47,6 +47,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User("Maria Brown", "maria@gmail.com", "48991435810", "12345", null);
         User u2 = new User("Alex Green", "alex@gmail.com", "48984067045", "54321", null);
 
--- src/main/java/com/conradoproject/course/entities/Product.java
+++ src/main/java/com/conradoproject/course/entities/Product.java
@@ -8,9 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
-
 
 @Entity
 @Table(name = "tb_product")
@@ -25,11 +27,11 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product(){
+    public Product() {
     }
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -108,7 +110,5 @@ public class Product implements Serializable {
             return false;
         return true;
     }
-
-    
 
 }
