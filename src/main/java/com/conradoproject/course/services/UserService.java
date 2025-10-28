a3a409d83364711975d8c0b5958709db088cb379
conradoovictor <conrado.victor01@gmail.com>
2025-10-28 15:53:52 -0300

    Exception handling - update

--- src/main/java/com/conradoproject/course/services/UserService.java
+++ src/main/java/com/conradoproject/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.conradoproject.course.repositories.UserRepository;
 import com.conradoproject.course.services.exceptions.DatabaseException;
 import com.conradoproject.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -33,21 +35,24 @@ public class UserService {
         return repository.save(obj);
     }
 
-    public void delete (long id){
+    public void delete(long id) {
         try {
             repository.deleteById(id);
-        }
-        catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
-        } catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
 
-    public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
